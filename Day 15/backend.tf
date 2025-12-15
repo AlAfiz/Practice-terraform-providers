3d8fa881f214aa63a9963a9009a06398cd1805db
zakariyaumuk@gmail.com <zakariyaumuk@gmail.com>
2025-12-15 19:47:31 +0100

    Revert "Day 14"
    This reverts commit d0646082a20a7873d539ebf178321b73ebb6a7b5.

--- Day 15/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "devopswithzacks-terraform-state"
-    key          = "dev/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
